4d145d5e5afe6520f8dd252f2c7be80d812d85ff
Deepika <deepikasowmya.5@gmail.com>
2023-10-08 20:57:28 +0530

    created new app

---
+++ AnimePlayer/AnimePlayer/AnimePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimePlayerApp.swift
+//  AnimePlayer
+//
+//  Created by Deepika Ponnaganti on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
